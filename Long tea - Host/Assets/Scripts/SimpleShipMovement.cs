1bc7a0616758c2bc3e709b96f6f2ae82883fdec3
Jenoah Kers <hello@jenoah.nl>
2022-02-08 14:41:06 +0100

    Added basic boat and water

---
+++ Long tea - Host/Assets/Scripts/SimpleShipMovement.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class SimpleShipMovement : MonoBehaviour
+{
+    private Rigidbody rb;
+    [SerializeField] private float moveSpeed = 3f;
+    [SerializeField] private float rotationSpeed = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();    
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        rb.AddTorque(transform.up * horizontal * rotationSpeed * Time.fixedDeltaTime);
+        rb.AddForce(transform.forward * vertical * moveSpeed * Time.fixedDeltaTime);
+    }
+}
