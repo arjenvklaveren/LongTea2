e3b8951ed793f125e3ea512049e272fdef11750f
Jenoah Kers <hello@jenoah.nl>
2022-02-14 10:59:13 +0100

    Fixed lobby not refreshing on client bug

--- Long tea - Host/Assets/Scripts/Networked/RoomPlayerUI.cs
+++ Long tea - Host/Assets/Scripts/Networked/RoomPlayerUI.cs
@@ -6,61 +6,13 @@ using TMPro;
 
 public class RoomPlayerUI : NetworkRoomPlayer
 {
+    [Header("Room listing")]
     [SerializeField] private GameObject UIPrefab;
     [SerializeField] private string playerListTransformName = "Playerlist";
 
     private Transform playerListTransform;
+    private List<NetworkRoomPlayer> playersInRoom;
+    
 
-    public override void OnStartClient()
-    {
-        if (isLocalPlayer)
-        {
-            base.OnStartClient();
-            playerListTransform = GameObject.Find(playerListTransformName).transform;
-        }
-    }
-
-    public override void OnClientEnterRoom()
-    {
-        base.OnClientEnterRoom();
-
-        if (isLocalPlayer)
-            StartCoroutine(TryDrawUI());
-    }
-
-    public override void OnClientExitRoom()
-    {
-        base.OnClientExitRoom();
-
-        if (isLocalPlayer)
-            StartCoroutine(TryDrawUI());
-    }
-
-    public IEnumerator TryDrawUI()
-    {
-        yield return new WaitForSeconds(0.1f);
-        NetworkRoomManager room = NetworkManager.singleton as NetworkRoomManager;
-        if (room)
-        {
-            if (!NetworkManager.IsSceneActive(room.RoomScene))
-                yield break;
-
-            foreach(Transform playerUIItem in playerListTransform)
-            {
-                Destroy(playerUIItem.gameObject);
-            }
-
-            List<NetworkRoomPlayer> playersInRoom = room.roomSlots;
-            for (int i = 0; i < playersInRoom.Count; i++)
-            {
-                CreatePlayerUIItem(playersInRoom[i]);
-            }
-        }
-    }
-
-    private void CreatePlayerUIItem(NetworkRoomPlayer playerInfo)
-    {
-        GameObject playerUI = Instantiate(UIPrefab, playerListTransform);
-        playerUI.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = $"Player {playerInfo.index}";
-    }
+    
 }
