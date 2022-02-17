6e68bb21119a96590f19fa7d374f028d539299ec
Arjen van Klaveren <arjenvklaveren@gmail.com>
2022-02-17 13:25:20 +0100

    Working ship using Liam model

--- Long tea - Host/Assets/Scripts/Ship/ShipShooting.cs
+++ Long tea - Host/Assets/Scripts/Ship/ShipShooting.cs
@@ -113,7 +113,7 @@ public class ShipShooting : MonoBehaviour
         predictionRenderer2.positionCount = (int)predictionPointAmount;
 
         List<Vector3> points = new List<Vector3>();
-        Vector3 startingPosition = selectedCannons[1].barrelTip.position;
+        Vector3 startingPosition = selectedCannons[1].barrelPivot.position;
         Vector3 startingVelocity = selectedCannons[1].barrelTip.transform.forward * shootPower;
         for (float t = 0; t < predictionPointAmount; t += predictionSmoothness / 2)
         {
